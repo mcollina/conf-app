0ecbd89c4c07d0aabaf7f71bf98b15a7b4939a12
Matteo Collina <hello@matteocollina.com>
2015-11-17 18:16:59 +0100

    Added step5.

---
+++ step5/lib/talks/data.js
@@ -0,0 +1,31 @@
+'use strict'
+
+module.exports = function build (db) {
+  const collection = db.collection('talks')
+
+  return {
+    put: put,
+    getById: getById,
+    list: list
+  }
+
+  function put (toStore, callback) {
+    collection.insert(toStore, function wrap (err, result) {
+      if (err) { return callback(err) }
+
+      callback(null, result.ops[0])
+    })
+  }
+
+  function getById (id, callback) {
+    collection.findOne({ _id: id }, callback)
+  }
+
+  function list (callback) {
+    collection.find().toArray(function wrap (err, result) {
+      if (err) { return callback(err) }
+
+      callback(null, result)
+    })
+  }
+}
